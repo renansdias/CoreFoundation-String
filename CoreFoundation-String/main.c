c93e951a2ade83afedc451fecb02d688c8056db7
Renan Dias <renansdiaz@gmail.com>
2014-04-22 20:40:55 +0100

    Initial Commit

---
+++ CoreFoundation-String/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  CoreFoundation-String
+//
+//  Created by Renan Dias on 22/04/2014.
+//  Copyright (c) 2014 Renan Dias. All rights reserved.
+//
+
+#include <CoreFoundation/CoreFoundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    CFShow(CFSTR("Hello, World!\n"));
+    return 0;
+}
+
